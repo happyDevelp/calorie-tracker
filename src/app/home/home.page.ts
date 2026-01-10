13e2adbd5b5c6aac88da56f2d89d03abe232a72f
Oles Malysh <olesmalish37@gmail.com>
2026-01-10 17:30:05 +0100

    add meals.selectors.ts and update home.page.ts

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,38 @@
-import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import {Component, inject, OnInit} from '@angular/core';
+import {Store} from '@ngrx/store';
+import {CommonModule} from '@angular/common';
+import {
+  IonHeader, IonToolbar, IonTitle, IonContent,
+} from '@ionic/angular/standalone';
+import {selectAllMeals} from '../state/meals.selectors';
+import {MealActions} from '../state/meals.actions';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  standalone: true,
+  imports: [
+    CommonModule, // Required for the 'async' pipe
+    IonHeader, IonToolbar, IonTitle, IonContent,
+  ],
 })
-export class HomePage {
-  constructor() {}
+
+export class HomePage implements OnInit {
+
+  private store = inject(Store);
+  meals$ = this.store.select(selectAllMeals);
+
+
+  // This is a method that is automatically called by Angular once when the component appears on the screen
+  ngOnInit() {
+    this.store.dispatch(MealActions.addMeal({
+      meal: {
+        id: Date.now().toString(),
+        title: 'Test Apple 🍎',
+        calories: 95,
+        date: new Date().toISOString()
+      }
+    }));
+  }
 }
---
+++ src/app/state/meals.selectors.ts
@@ -0,0 +1,11 @@
+import { createFeatureSelector, createSelector } from '@ngrx/store';
+import { MealsState } from './meals.reducer';
+
+// 1. Select the entire "meals" feature state from the global store
+export const selectMealsState = createFeatureSelector<MealsState>('meals');
+
+// 2. Select the specific "meals" array for our UI components
+export const selectAllMeals = createSelector(
+  selectMealsState,
+  (state: MealsState) => state.meals // Returns only the list of meal objects
+);
